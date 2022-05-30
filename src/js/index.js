a1033cb81bfe912fe183dba2eb42437e6dcb74e3
Pedro Luis Sanchez Calle <psanchezc09@gmail.com>
2022-05-30 13:00:15 -0500

    Se renombro maqueta.html a inde.php

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+
+var menuResponsive = document.getElementById('mobile-menu');
+var menuSidebar=document.getElementById('');
+var btnOpen = document.getElementById('iconOpen');
+var btnClose = document.getElementById('iconClose');
+
+function hideShow() {
+    if (menuResponsive.classList.contains('hidden')) {
+        menuResponsive.classList.remove('hidden');
+        btnOpen.classList.add('hidden');
+        btnClose.classList.remove('hidden');
+    } else {
+        menuResponsive.classList.add('hidden');
+        btnClose.classList.add('hidden');
+        btnOpen.classList.remove('hidden');
+    }
+    
+}
+
+
+
+
+const btnMenu = document.getElementById('bntResponsive');
+btnMenu.addEventListener('click', hideShow);
