0268187fe1e78bd07e6e0e26dc666b71c0a19941
Pedro Luis Sanchez Calle <psanchezc09@gmail.com>
2022-05-30 18:36:16 -0500

    Ocultar sidebar agregado

--- src/index.php
+++ src/index.php
@@ -21,7 +21,7 @@
 
 
             <!-- ENG Botón responsive -->
-            <button id="bntResponsive" class="p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-white" data-collapse-toggle="mobile-menu" type="button"  aria-controls="mobile-menu" aria-expanded="false">
+            <button id="bntResponsive" class="p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-white" data-collapse-toggle="" type="button"  aria-controls="" aria-expanded="false">
                 <svg id="iconOpen" class="text-white w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                 <svg id="iconClose" class="text-white hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
--- src/js/index.js
+++ src/js/index.js
@@ -3,18 +3,47 @@ var menuResponsive = document.getElementById('mobile-menu');
 var menuSidebar=document.getElementById('');
 var btnOpen = document.getElementById('iconOpen');
 var btnClose = document.getElementById('iconClose');
-
+var menuSide=document.getElementById('sideBarList');
+var btnOpen1 = document.getElementById('iconOpen1');
+var btnClose1 = document.getElementById('iconClose1');
+var spanMenu=document.getElementById('spMenu');
+var curso=document.getElementById('curso');
 function hideShow() {
     if (menuResponsive.classList.contains('hidden')) {
         menuResponsive.classList.remove('hidden');
         btnOpen.classList.add('hidden');
         btnClose.classList.remove('hidden');
+        
     } else {
         menuResponsive.classList.add('hidden');
         btnClose.classList.add('hidden');
         btnOpen.classList.remove('hidden');
+      
     }
     
+}
+
+function sidebarShow() {
+    if (menuSide.classList.contains('hidden')) {
+        menuSide.classList.remove('hidden');
+        btnOpen1.classList.add('hidden');
+        btnClose1.classList.remove('hidden');
+       spanMenu.classList.remove('hidden');
+    
+    } else {
+        console.log('Visible');
+        menuSide.classList.add('hidden');
+        btnClose1.classList.add('hidden');
+        btnOpen1.classList.remove('hidden');
+        spanMenu.classList.add('hidden');
+        
+    }
+    
+
+
+    
+ 
+   
 }
 
 
@@ -22,3 +51,8 @@ function hideShow() {
 
 const btnMenu = document.getElementById('bntResponsive');
 btnMenu.addEventListener('click', hideShow);
+
+
+const btnsidebar = document.getElementById('bntSidebar');
+btnsidebar.addEventListener('click',sidebarShow  );
+
