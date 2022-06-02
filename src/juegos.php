cc4caac266b2dfa4b699493e2f356c89e58801d6
Pedro Luis Sanchez Calle <psanchezc09@gmail.com>
2022-06-02 11:41:01 -0500

    Vista sección de juegos

---
+++ src/juegos.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juegos</title>
+    <link href="../dist/output.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+</head>
+
+<body>
+
+    <?php require_once('./navbar.html') ?>
+    <div class="flex bg-gray-400 py-5">
+
+        <?php require_once('./sidebar.html') ?>
+        <div class=" bg-white container mx-auto px-5 py-4 rounded">
+
+
+
+
+            <h1 class="text-center ">Mis Juegos</h1>
+            <br />
+
+
+            <div class="grid md:grid-cols-2 sm:grid-cols-2 gap-5">
+                <div>
+                    <div class="shadow-md p-3">
+                        <div><img src="images/juegos/callOf.jpg" alt=""></div>
+                        <h3>Call of Duty</h3>
+                        <p class="text-sm">
+                            Ve cara a cara figuras históricas y duras verdades mientras batallas por el mundo en localizaciones icónicas como Berlín del Este, Vietnam, el cuartel general soviético de la KGB y muchos más.
+                        </p>
+                    </div>
+                    <!-- <div class="max-w-md mx-auto bg-white rounded-lg shadow-xl overflow-hidden sm:max-w-2xl ring-1 ring-slate-900/5"><div class="flex flex-col sm:flex-row"><div class="sm:shrink-0"><img class="w-full sm:w-[193px] h-[200px] object-cover object-bottom sm:object-center" src="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=404&amp;q=80" alt="Beautiful abstract building in the sun" width="202" height="192"></div><div class="p-6 2xl:p-8 space-y-2"><div class="font-medium text-sm leading-6 text-indigo-600">Company retreats</div><a href="#" class="block font-semibold text-base text-slate-900 leading-6 hover:underline">Incredible accomodation for your team</a><p class="text-sm text-slate-600 leading-6">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that </p></div></div></div> -->
+                </div>
+                <div>
+                    <div class="shadow-md p-3">
+                        <div><img src="images/juegos/oper.jpg" alt=""></div>
+                        <h3>Operación 7</h3>
+                        <p class="text-sm">
+                            Un juego shooter multijugador en primera persona desarrollado por Park ESM y portado para latinoamérica por Axeso5.
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <div class="shadow-md p-3">
+                        <div><img src="images/juegos/pes22.jpg" alt=""></div>
+                        <h3>eFootball™ 2022</h3>
+                        <p class="text-sm">
+                            La esencia de eFootball™ es "eFootball™ World".
+                            Juega usando alguna de las grandes potencias futbolísticas con "Equipo auténtico" o ficha y desarrolla a tus jugadores favoritos para formar tu "Equipo ideal".
+                            ¡Enfréntate a usuarios de todo el mundo y participa en diversos eventos!
+                        </p>
+                    </div>
+                </div>
+
+                <div class="shadow-md p-3">
+                    <div><img src="images/juegos/TheAuto.png" alt=""></div>
+                    <h3>Grand Theft Auto V</h3>
+                    <p class="text-sm">
+                        Grand Theft Auto V es un videojuego de acción-aventura de mundo abierto desarrollado por el estudio Rockstar North y distribuido por Rockstar Games. Fue lanzado el 17 de septiembre de 2013 para las consolas PlayStation 3 y Xbox 360.​
+                    </p>
+                </div>
+                <!-- <div class="max-w-md mx-auto bg-white rounded-lg shadow-xl overflow-hidden sm:max-w-2xl ring-1 ring-slate-900/5"><div class="flex flex-col sm:flex-row"><div class="sm:shrink-0"><img class="w-full sm:w-[193px] h-[200px] object-cover object-bottom sm:object-center" src="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=404&amp;q=80" alt="Beautiful abstract building in the sun" width="202" height="192"></div><div class="p-6 2xl:p-8 space-y-2"><div class="font-medium text-sm leading-6 text-indigo-600">Company retreats</div><a href="#" class="block font-semibold text-base text-slate-900 leading-6 hover:underline">Incredible accomodation for your team</a><p class="text-sm text-slate-600 leading-6">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that </p></div></div></div> -->
+            </div>
+        </div>
+
+
+    </div>
+
+    </div>
+
+
+    <?php require_once('./footer.html') ?>
+
+    <script type="text/javascript" src="./js/index.js"></script>
+
+</body>
+
+</html>
