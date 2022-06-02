41de41ebe87ba4b5f3e60aef302ab7382161c187
Pedro Luis Sanchez Calle <psanchezc09@gmail.com>
2022-06-02 11:48:15 -0500

    Funcionalidades completas

--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,58 @@
-
+console.log('Llamdo');
 var menuResponsive = document.getElementById('mobile-menu');
-var menuSidebar=document.getElementById('');
+var sideBar = document.getElementById('sideBar');
+var btnOpenMenu = document.getElementById('menuOpen');
 var btnOpen = document.getElementById('iconOpen');
 var btnClose = document.getElementById('iconClose');
-var menuSide=document.getElementById('sideBarList');
-var btnOpen1 = document.getElementById('iconOpen1');
-var btnClose1 = document.getElementById('iconClose1');
-var spanMenu=document.getElementById('spMenu');
-var curso=document.getElementById('curso');
+var btnCloseMenu = document.getElementById('menuClose');
+
 function hideShow() {
     if (menuResponsive.classList.contains('hidden')) {
         menuResponsive.classList.remove('hidden');
         btnOpen.classList.add('hidden');
         btnClose.classList.remove('hidden');
-        
+
     } else {
         menuResponsive.classList.add('hidden');
         btnClose.classList.add('hidden');
         btnOpen.classList.remove('hidden');
-      
+
     }
-    
+
 }
 
-function sidebarShow() {
-    if (menuSide.classList.contains('hidden')) {
-        menuSide.classList.remove('hidden');
-        btnOpen1.classList.add('hidden');
-        btnClose1.classList.remove('hidden');
-       spanMenu.classList.remove('hidden');
-    
+function Openbar() {
+    if (sideBar.classList.contains('hidden')) {
+        console.log("Oculto");
+        btnOpenMenu.classList.add('hidden');
+
+        sideBar.classList.remove('hidden');
+        btnCloseMenu.classList.remove('hidden');
+        btnCloseMenu.classList.add('visible');
+
+
+
+
     } else {
-        console.log('Visible');
-        menuSide.classList.add('hidden');
-        btnClose1.classList.add('hidden');
-        btnOpen1.classList.remove('hidden');
-        spanMenu.classList.add('hidden');
-        
+        console.log("visible");
+        btnOpenMenu.classList.remove('hidden');
+
+        sideBar.classList.add('hidden');
+        btnCloseMenu.classList.remove('visible');
+        btnCloseMenu.classList.add('hidden');
+
     }
-    
 
-
-    
- 
-   
 }
+
+
 
 
 
 
 const btnMenu = document.getElementById('bntResponsive');
 btnMenu.addEventListener('click', hideShow);
+btnOpenMenu.addEventListener('click', Openbar);
+btnCloseMenu.addEventListener('click',Openbar);
 
-
-const btnsidebar = document.getElementById('bntSidebar');
-btnsidebar.addEventListener('click',sidebarShow  );
 
